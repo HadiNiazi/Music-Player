11857a08bace13ae72152daa3ddcebc250e3faca
Hadayat Niazi <hadayat@pakcr.org.pk>
2025-06-29 02:54:34 +0500

    added html & css

---
+++ src/components/MusicPlayer.vue
@@ -0,0 +1,26 @@
+<template>
+    <div>
+        
+        <h2>🎵 My Playlist</h2>
+
+        <div class="form-inline">
+            <input id="title" placeholder="🎵 Title">
+            <input id="artist" placeholder="🎤 Artist">
+            <input id="audio" placeholder="🔗 Audio URL">
+            <button>➕ Add Song</button>
+        </div>
+
+        <ul id="playlist" class="playlist"></ul>
+
+    </div>
+</template>
+
+<script>
+export default {    
+
+}
+</script>
+
+<style scoped>
+
+</style>    
